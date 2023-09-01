d7a6452dcb615660352baa879d67a62c46fe5f63
Priydarshini Karanth <kpriya1203@gmail.com>
2023-09-01 15:27:20 +0530

    custom css classes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import heroImg from '../assets/hero.svg';
 const Hero = () => {
   return (
     <div className="bg-sky-100 py-24">
-      <div className="mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8">
+      <div className="align-element grid md:grid-cols-2 items-center gap-8">
         <article>
           <h1 className="text-7xl font-bold tracking-wider">I'm Betty</h1>
           <p className="text-3xl mt-4 text-slate-700 capitalize tracking-wide">
@@ -15,13 +15,13 @@ const Hero = () => {
           </p>
           <div className="flex gap-x-4 mt-4">
             <a href="#">
-              <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+              <FaGithubSquare className="icon" />
             </a>
             <a href="#">
-              <FaLinkedin className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+              <FaLinkedin className="icon" />
             </a>
             <a href="#">
-              <FaTwitterSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+              <FaTwitterSquare className="icon" />
             </a>
           </div>
         </article>
