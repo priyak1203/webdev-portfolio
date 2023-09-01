03e4c9ee45dc6d5bd44771b55fbcc4cca3227c1c
Priydarshini Karanth <kpriya1203@gmail.com>
2023-09-01 16:01:58 +0530

    skills section

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,9 @@
+const SectionTitle = ({ text }) => {
+  return (
+    <div className="border-b border-gray-300 pb-5">
+      <h2 className="text-3xl font-medium capitalize tracking-wider">{text}</h2>
+    </div>
+  );
+};
+
+export default SectionTitle;
--- src/data.js
+++
@@ -1,8 +0,0 @@
-import { nanoid } from 'nanoid';
-
-export const links = [
-  { id: nanoid(), href: '#home', text: 'home' },
-  { id: nanoid(), href: '#skills', text: 'skills' },
-  { id: nanoid(), href: '#about', text: 'about' },
-  { id: nanoid(), href: '#projects', text: 'projects' },
-];
